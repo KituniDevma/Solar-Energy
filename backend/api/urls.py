fa1d3b9d41c9e0fa75fba9f6e9b8bc99bed21ea8
Tharindu Wijekoon <wijekoontharindu80@gmail.com>
2024-09-05 23:55:36 +0530

    model implemented in backend

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('location/', views.LocationListCreate.as_view(), name='location-list'),
     path('location/delete/<int:pk>/', views.LocationDelete.as_view(), name='delete-location'),
     path('weather/', views.get_weather, name='get-weather'),
-    path('data/', views.get_data, name='get-data')
+    path('data/', views.get_data, name='get-data'),
+    path('model/', views.run_model, name='run-model')
 ]
